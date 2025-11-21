da43fa74e37e5447ec50d0e7265f7d40133da7ee
Andersonpaipa <paipadiazandersonduvan@gmail.com>
2025-11-20 23:59:11 -0500

    Update codigo_mongo.js

--- codigo_mongo.js
+++ codigo_mongo.js
@@ -26,6 +26,45 @@ db.owners.insertMany([
 ]);
 
 // --- Inserción de Mascotas (100 documentos) ---
+db.pets.insertMany([
+  { _id: 1, owner_id: 1, name: "Firulais", species: "Perro", breed: "Labrador", age: 4 },
+  { _id: 2, owner_id: 1, name: "Nina", species: "Gato", breed: "Siames", age: 2 },
+  { _id: 3, owner_id: 1, name: "Bruno", species: "Perro", breed: "Pastor Alemán", age: 3 },
+  { _id: 4, owner_id: 1, name: "Coco", species: "Gato", breed: "Criollo", age: 1 },
+
+  { _id: 5, owner_id: 2, name: "Rocky", species: "Perro", breed: "Pitbull", age: 5 },
+  { _id: 6, owner_id: 2, name: "Pelusa", species: "Gato", breed: "Angora", age: 3 },
+  { _id: 7, owner_id: 2, name: "Bobby", species: "Perro", breed: "Beagle", age: 4 },
+  { _id: 8, owner_id: 2, name: "Misu", species: "Gato", breed: "Persa", age: 2 },
+
+  { _id: 9, owner_id: 3, name: "Max", species: "Perro", breed: "Criollo", age: 6 },
+  { _id: 10, owner_id: 3, name: "Kira", species: "Gato", breed: "Persa", age: 1 },
+  { _id: 11, owner_id: 3, name: "Rex", species: "Perro", breed: "Labrador", age: 2 },
+  { _id: 12, owner_id: 3, name: "Molly", species: "Gato", breed: "Siames", age: 4 },
+
+  { _id: 13, owner_id: 4, name: "Loki", species: "Perro", breed: "Husky", age: 2 },
+  { _id: 14, owner_id: 4, name: "Luna", species: "Gato", breed: "Bengalí", age: 4 },
+  { _id: 15, owner_id: 4, name: "Thor", species: "Perro", breed: "Doberman", age: 3 },
+  { _id: 16, owner_id: 4, name: "Nala", species: "Gato", breed: "Criollo", age: 5 },
+
+  { _id: 17, owner_id: 5, name: "Toby", species: "Perro", breed: "Beagle", age: 7 },
+  { _id: 18, owner_id: 5, name: "Milo", species: "Gato", breed: "Criollo", age: 2 },
+  { _id: 19, owner_id: 5, name: "Spike", species: "Perro", breed: "Pitbull", age: 1 },
+  { _id: 20, owner_id: 5, name: "Lola", species: "Gato", breed: "Bengalí", age: 3 },
+
+  { _id: 21, owner_id: 6, name: "Zeus", species: "Perro", breed: "Rottweiler", age: 6 },
+  { _id: 22, owner_id: 6, name: "Daisy", species: "Gato", breed: "Persa", age: 4 },
+  { _id: 23, owner_id: 6, name: "Oreo", species: "Perro", breed: "Pug", age: 2 },
+  { _id: 24, owner_id: 6, name: "Mora", species: "Gato", breed: "Angora", age: 1 },
+
+  { _id: 25, owner_id: 7, name: "Simba", species: "Perro", breed: "Labrador", age: 5 },
+  { _id: 26, owner_id: 7, name: "Cleo", species: "Gato", breed: "Criollo", age: 4 },
+  { _id: 27, owner_id: 7, name: "Chester", species: "Perro", breed: "Beagle", age: 2 },
+  { _id: 28, owner_id: 7, name: "Katy", species: "Gato", breed: "Siames", age: 3 },
+
+  { _id: 29, owner_id: 8, name: "Buddy", species: "Perro", breed: "Pastor Alemán", age: 3 },
+  { _id: 30, owner_id: 8, name: "Arya", species: "Gato", breed: "Bengalí", age: 2 },
+ { _id: 31, owner_id: 11, name: "Quesito", species: "Perro", breed: "Labrador", age: 2, owner_name: "Karen Garzón" },
 var speciesList = ["Perro", "Gato"];
 var breedsPerro = ["Labrador", "Pug", "Pastor Alemán", "Golden", "Bulldog"];
 var breedsGato = ["Siamés", "Persa", "Bengala", "Criollo"];
@@ -89,3 +128,4 @@ db.pets.aggregate([
   { $sort: { total: -1 } },
   { $limit: 5 }
 ]);
+
